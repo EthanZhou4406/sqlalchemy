bf37964b920456a0e7d3cf5ada27a6b8ce1f8acc
ZhouXiang周翔 <xiang.zhou1@CALB-TECH.COM>
2022-11-03 17:28:24 +0800

    update temp

--- temp/forcrm.py
+++ temp/forcrm.py
@@ -2,35 +2,31 @@ from sqlalchemy import String,Column,Table,Integer,create_engine,Text,text,DateT
 
 from sqlalchemy.orm import registry,mapper
 
-engine = create_engine('mysql+pymysql://root:123456@127.0.0.1:3306/testdb')
+localdb = 'mysql+pymysql://root:123456@127.0.0.1:3306/testdb'
+crmdb = 'mysql+pymysql://crm_v1:crm123@10.40.6.253:13306/crm_v1'
+engine = create_engine(localdb)
 con = registry()
 
-crm_fault_tree = Table('rms_fault_tree', con.metadata,
-                       Column('id', Integer, primary_key=True, autoincrement=True, comment='id'),
-                       Column('pid', Integer, comment='父id'),
-                       Column('tag', String(10), comment='复制tag'),
-                       Column('description', String(255), nullable=False, comment='描述'),
-                       Column('mechanism', Text(16000000), nullable=False, comment='机理'),
-                       Column('img_id', String(255), comment='照片ID'),
-                       Column('appendix_id', String(255), comment='附件ID'),
-                       Column('sort_num', Float(32), comment='节点顺序'),
-                       Column('status', Integer, server_default=text('1'), comment='状态（-1：删除，0：禁用，1：启用）'),
-                       Column('source', Integer, server_default=text('0'), comment='来源（0：新增 1：继承）'),
-                       Column('create_time', DateTime(20), comment='创建时间'),
-                       Column('update_time', DateTime(20), comment='更新时间'),
-                       Column('delete_time', DateTime(20), comment='删除时间'),
-                       Column('create_user', Integer, comment='创建者id'),
-                       comment='故障树基础信息表'
-                       )
+crm_dict = Table('crm_dict', con.metadata,
+                      Column('id', Integer, primary_key=True, autoincrement=True, comment='选项id'),
+                      Column('pid', Integer, comment='父级id'),
+                      Column('desc', String(255), comment='字典值'),
+                      Column('create_time', DateTime(20), comment='创建时间'),
+                      Column('update_time', DateTime(20), comment='更新时间'),
+                      Column('delete_time', DateTime(20), comment='删除时间'),
+                      Column('create_user', Integer, comment='创建者id'),
+                      Column('remark', String(255), comment='备注'),
+                      comment='字典选项表'
+                      )
 
 
-class CRM_FAULT_TREE(object):
+class CRM_DICT(object):
     def __init__(self, **kwargs):
         for i in kwargs:
             self.__dict__[i] = kwargs.get(i)
 
 
 # table与model的映射
-mapper(CRM_FAULT_TREE, crm_fault_tree)
+mapper(CRM_DICT, crm_dict)
 
 con.metadata.create_all(bind=engine)
